451494a3b5d7060d70db22a2e80a49d054474682
Sebastian Dowell <sebastiandowell@Sebastians-MacBook-Air.local>
2023-02-13 19:44:58 -0800

    Added blinking & possible future /commands/LEDcommand.java

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -2,11 +2,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.concurrent.TimeUnit;
 
 public class LED extends SubsystemBase{
     public AddressableLED LEDstrip;
     public AddressableLEDBuffer LEDBuffer;
     public int rainbowFirstPixelHue;
+    public boolean blinkStatus;
     public LED() {
         LEDstrip = new AddressableLED(3);
         LEDBuffer = new AddressableLEDBuffer(300);
@@ -20,23 +22,32 @@ public class LED extends SubsystemBase{
         LEDstrip.stop();
     }
     public void setLED(int r, int g, int b) {
-        for (var i = 0; i < LEDBuffer.getLength(); i++) {
+        for (int i = 0; i < LEDBuffer.getLength(); i++) {
             LEDBuffer.setRGB(i, r, g, b);
         }
         LEDstrip.setData(LEDBuffer);
-    }
-    public void setYellow() {
-        setLED(255, 255, 0);
     }
     public void setPurple() {
         setLED(160, 32, 240);
     }
     public void rainbow() {
-        for (var i = 0; i < LEDBuffer.getLength(); i++) {
+        for (int i = 0; i < LEDBuffer.getLength(); i++) {
             final var hue = (rainbowFirstPixelHue + (i * 180 / LEDBuffer.getLength())) % 180;
             LEDBuffer.setHSV(i, hue, 255, 128);
         }
         rainbowFirstPixelHue += 3;
         rainbowFirstPixelHue %= 180;
-      }
-}   
+    }
+    public void startBlink() throws InterruptedException {
+        while (blinkStatus = true) {
+            stopLED();
+            TimeUnit.SECONDS.sleep((long) 0.5);
+            startLED();
+            TimeUnit.SECONDS.sleep((long) 0.5);
+        }
+    }
+    public void stopBlink() throws InterruptedException {
+        blinkStatus = false;
+        stopLED();
+    }
+}
