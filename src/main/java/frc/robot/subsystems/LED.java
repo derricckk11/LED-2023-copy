e25f48324cbcfa66fcf36ab191a77d2fdbc29aff
c-bassx <100545917+c-bassx@users.noreply.github.com>
2023-02-14 20:14:34 -0800

    Update LED.java

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -9,27 +9,33 @@ public class LED extends SubsystemBase{
     public AddressableLEDBuffer LEDBuffer;
     public int rainbowFirstPixelHue;
     public boolean blinkStatus;
+
     public LED() {
         LEDstrip = new AddressableLED(3);
         LEDBuffer = new AddressableLEDBuffer(300);
         LEDstrip.setLength(LEDBuffer.getLength());
         LEDstrip.setData(LEDBuffer);
     }
+
     public void startLED(){
         LEDstrip.start();
     }
+
     public void stopLED(){
         LEDstrip.stop();
     }
+
     public void setLED(int r, int g, int b) {
         for (int i = 0; i < LEDBuffer.getLength(); i++) {
             LEDBuffer.setRGB(i, r, g, b);
         }
         LEDstrip.setData(LEDBuffer);
     }
+
     public void setPurple() {
         setLED(160, 32, 240);
     }
+
     public void rainbow() {
         for (int i = 0; i < LEDBuffer.getLength(); i++) {
             final var hue = (rainbowFirstPixelHue + (i * 180 / LEDBuffer.getLength())) % 180;
@@ -38,6 +44,7 @@ public class LED extends SubsystemBase{
         rainbowFirstPixelHue += 3;
         rainbowFirstPixelHue %= 180;
     }
+
     public void startBlink() throws InterruptedException {
         while (blinkStatus = true) {
             stopLED();
@@ -46,6 +53,7 @@ public class LED extends SubsystemBase{
             TimeUnit.SECONDS.sleep((long) 0.5);
         }
     }
+
     public void stopBlink() throws InterruptedException {
         blinkStatus = false;
         stopLED();
